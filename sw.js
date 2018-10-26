e647328fb777527c255b5b1dd5c369030b1faaea
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2018-10-26 15:22:05 +0530

    fix: Optimize for the browser support
    Changing the link for the normalize.css for the default link to the link from cdnjs. And adding more attributes to the script tag.

--- sw.js
+++ sw.js
@@ -1,18 +1,33 @@
-var staticCacheName = 'mws-static-v1';
+var staticCacheName = 'restaurant-cache';
+
+let urlsToCache = [
+    './',
+    './index.html',
+    './restaurant.html',
+    './css/styles.css',
+    './data/restaurants.json',
+    './js/dbhelper.js',
+    './js/main.js',
+    './js/restaurant_info.js',
+    './js/sw_register.js',
+    './img/1.jpg',
+    './img/2.jpg',
+    './img/3.jpg',
+    './img/4.jpg',
+    './img/5.jpg',
+    './img/6.jpg',
+    './img/7.jpg',
+    './img/8.jpg',
+    './img/9.jpg',
+    './img/10.jpg'
+];
 
 /**
  * Install Service Worker
  */
 self.addEventListener('install', (event) => {
     event.waitUntil(caches.open(staticCacheName).then((cache) => {
-        return cache.addAll(['./',
-            'js/main.js', 'js/restaurant_info.js', 'js/dbhelper.js', 'js/indexController.js',
-            'css/styles.css',
-            'img/1.jpg', 'img/2.jpg', 'img/3.jpg', 'img/4.jpg', 'img/5.jpg', 'img/6.jpg', 'img/7.jpg', 'img/8.jpg', 'img/9.jpg', 'img/10.jpg',
-            'data/restaurants.json',
-            'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
-            'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js',
-        ]);
+        return cache.addAll(urlsToCache);
     }));
 });
 
@@ -22,7 +37,7 @@ self.addEventListener('install', (event) => {
 self.addEventListener('activate', (event) => {
     event.waitUntil(caches.keys().then((cacheNames) => {
         return Promise.all(cacheNames.filter((cacheName) => {
-            return cacheName.startsWith('mws-') && cacheName != staticCacheName;
+            return cacheName.startsWith('restaurant-') && cacheName != staticCacheName;
         }).map((cacheName) => {
             return caches.delete(cacheName);
         }));
