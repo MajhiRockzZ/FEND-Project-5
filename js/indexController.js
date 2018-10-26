2498acc3e2f03d4bb1a1eb6dd5823d0578937199
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2018-10-26 09:31:01 +0530

    feat: Check for waiting worker
    Check if there is a waiting worker if so then inform the user about the update

--- js/indexController.js
+++ js/indexController.js
@@ -19,12 +19,12 @@ IndexController.prototype._registerServiceWorker = function () {
      */
     if (!navigator.serviceWorker) return;
 
-    let IndexController = this;
+    let indexController = this;
 
     /**
      * Register new service worker
      */
-    navigator.serviceWorker.register('./sw.js').then(reg) => {
+    navigator.serviceWorker.register('./sw.js').then((reg) => {
         console.log('Service Worker successfully registered with scope : ', reg.scope);
 
         /**
@@ -33,5 +33,13 @@ IndexController.prototype._registerServiceWorker = function () {
         if (!navigator.serviceWorker.controller) {
             return;
         }
-    }
-}
+
+        /**
+         * Check for WAITING WORKER
+         */
+        if (reg.waiting) {
+            indexController._updateReady(reg.waiting);
+            return;
+        }
+    });
+};
